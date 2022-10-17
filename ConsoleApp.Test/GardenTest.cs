b0cdb51a4a0e366eacf74c93ff51a1e024230222
Paweł Łęczycki <pawel@msd.info.pl>
2022-10-17 12:47:28 +0200

    TDD

--- ConsoleApp.Test/GardenTest.cs
+++ ConsoleApp.Test/GardenTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleApp.Test
@@ -48,6 +49,132 @@ namespace ConsoleApp.Test
 
             //Assert
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("", 1)]
+        [DataRow("  ", 1)]
+        [DataRow(null, 1)]
+        [DataRow("a", 0)]
+        public void Plant_InvalidName_NotAddedToGarden(string name, int size)
+        {
+            //Arrange
+            var garden = new Garden(size);
+
+            //Act
+            bool result;
+            try
+            {
+                result = garden.Plant(name);
+            }
+            catch
+            {
+                result = false;
+            }
+
+            //Assert
+            Assert.IsFalse(result);
+            Assert.IsFalse(garden.GetPlants().Contains(name));
+        }
+
+        [TestMethod]
+        public void Plant_NotEnoughSpace_False()
+        {
+            //Arrange
+            string name = new Fixture().Create<string>();
+            const int VALID_SIZE = 0;
+            var garden = new Garden(VALID_SIZE);
+
+            //Act
+            var result = garden.Plant(name);
+
+            //Assert
+            Assert.IsFalse(result);
+            //Assert.IsFalse(garden.GetPlants().Contains(name));
+        }
+
+        [TestMethod]
+        public void Plant_Null_ArgumentNullException()
+        {
+            //Arrange
+            const string NULL_NAME = null;
+            const int VALID_SIZE = 0;
+            var garden = new Garden(VALID_SIZE);
+
+            //Act
+            Action action = () => garden.Plant(NULL_NAME);
+
+            //Assert
+            var exception = Assert.ThrowsException<ArgumentNullException>(action);
+            Assert.AreEqual("plant", exception.ParamName);
+            //Assert.IsFalse(garden.GetPlants().Contains(NULL_NAME));
+        }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("   ")]
+        [DataRow("\t")]
+        public void Plant_InvalidName_ArgumentException(string invalidName)
+        {
+            //Arrange
+            const int VALID_SIZE = 0;
+            var garden = new Garden(VALID_SIZE);
+
+            //Act
+            Action action = () => garden.Plant(invalidName);
+
+            //Assert
+            var exception = Assert.ThrowsException<ArgumentException>(action);
+            Assert.AreEqual("plant", exception.ParamName);
+            Assert.IsTrue(exception.Message.Contains("Invalid name"));
+        }
+
+        [TestMethod]
+        public void Plant_DuplicatedName_ArgumentException()
+        {
+            //Arrange
+            const int VALID_SIZE = 1;
+            string name = new Fixture().Create<string>();
+            var garden = new Garden(VALID_SIZE);
+            garden.Plant(name);
+
+            //Act
+            Action action = () => garden.Plant(name);
+
+            //Assert
+            var exception = Assert.ThrowsException<ArgumentException>(action);
+            Assert.AreEqual("plant", exception.ParamName);
+            Assert.IsTrue(exception.Message.Contains("Duplicated name"));
+        }
+
+        [TestMethod]
+        public void Plant_Name_AddedToGarden()
+        {
+            //Arrange
+            string name = new Fixture().Create<string>();
+            const int VALID_SIZE = 1;
+            var garden = new Garden(VALID_SIZE);
+
+            //Act
+            garden.Plant(name);
+
+            //Assert
+            Assert.IsTrue(garden.GetPlants().Contains(name));
+        }
+
+        [TestMethod]
+        public void GetPlants_CopyOfPlantsCollection()
+        {
+            //Arrange
+            const int VALID_SIZE = 0;
+            var garden = new Garden(VALID_SIZE);
+
+            //Act
+            var result1 = garden.GetPlants();
+            var result2 = garden.GetPlants();
+
+            //Assert
+            Assert.AreNotSame(result1, result2);
         }
     }
 }
--- ConsoleApp/Garden.cs
+++ ConsoleApp/Garden.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp.Test
 {
     public class Garden
     {
+        private ICollection<string> _plants = new List<string>();
+
         public Garden(int size)
         {
             if (size < 0)
@@ -13,9 +17,40 @@ namespace ConsoleApp.Test
 
         public int Size { get; }
 
-        public bool Plant(string name)
+        public bool Plant(string plant)
         {
+            CheckForNull(plant);
+            ValidateName(plant);
+            CheckForDuplicates(plant);
+            if (Size <= _plants.Count)
+            {
+                return false;
+            }
+            _plants.Add(plant);
             return true;
+        }
+
+        private void CheckForDuplicates(string plant)
+        {
+            if (_plants.Contains(plant))
+                throw new ArgumentException("Duplicated name", nameof(plant));
+        }
+
+        private static void ValidateName(string plant)
+        {
+            if (string.IsNullOrWhiteSpace(plant))
+                throw new ArgumentException("Invalid name", nameof(plant));
+        }
+
+        private static void CheckForNull(string plant)
+        {
+            if (plant == null)
+                throw new ArgumentNullException(nameof(plant));
+        }
+
+        public IEnumerable<string> GetPlants()
+        {
+            return _plants.ToList();
         }
     }
 }
