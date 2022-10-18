80b3f2bb768a9a55f83de9c40fdca944b5889ef3
Paweł Łęczycki <pawel@msd.info.pl>
2022-10-18 10:59:32 +0200

    WebApi TDD Mock

---
+++ WebApi.MockDemo.Test/Controllers/UsersControllerTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Models;
+using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.MockDemo.Controllers;
+
+namespace WebApi.MockDemo.Test.Controllers
+{
+    [TestClass]
+    public class UsersControllerTest
+    {
+        [TestMethod]
+        public void Get_OkWithAllUsers()
+        {
+            //Arrange
+            var service = new Mock<ICrudService<User>>();
+            var expectedUsers = new Fixture().CreateMany<User>();
+            service.Setup(x => x.Read())
+                .Returns(expectedUsers);
+
+            var controller = new UsersController(service.Object);
+            
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            Assert.IsInstanceOfType(okResult.Value, typeof(IEnumerable<User>));
+            var resultUsers = (IEnumerable<User>)okResult.Value;
+            Assert.AreEqual(expectedUsers, resultUsers);
+        }
+
+        [TestMethod]
+        public void Get_ExistingId_OkWithUser()
+        {
+            //Arrange
+            var service = new Mock<ICrudService<User>>();
+            var expectedUser = new Fixture().Create<User>();
+
+            service.Setup(x => x.Read(expectedUser.Id))
+                .Returns(expectedUser);
+
+            var controller = new UsersController(service.Object);
+
+            //Act
+            var result = controller.Get(expectedUser.Id);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            Assert.IsInstanceOfType(okResult.Value, typeof(User));
+            Assert.AreEqual(expectedUser, (User)okResult.Value);
+            service.Verify(x => x.Read(It.IsAny<int>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void Get_NotExistingId_NotFound()
+        {
+            //Arrange
+            var service = new Mock<ICrudService<User>>();
+            var id = new Fixture().Create<int>();
+
+            service.Setup(x => x.Read(id))
+                .Returns((User)null);
+
+            var controller = new UsersController(service.Object);
+
+            //Act
+            var result = controller.Get(id);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            service.Verify();
+        }
+    }
+}
---
+++ WebApi.MockDemo/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.MockDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ICrudService<User> _service;
+
+        public UsersController(ICrudService<User> service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Get()
+        {
+            var result = _service.Read();
+            return Ok(result);
+        }
+
+        public IActionResult Get(int id)
+        {
+            var result = _service.Read(id);
+            if (result == null)
+                return NotFound();
+            return Ok(result);
+        }
+    }
+}
